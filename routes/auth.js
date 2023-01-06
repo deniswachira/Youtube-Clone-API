148a6084bacc7091749aa770b6d03b1feb09754b
Denis Wachira <deniswachira77@gmail.com>
2023-01-07 00:46:44 +0300

    token config

--- routes/auth.js
+++ routes/auth.js
@@ -25,13 +25,13 @@ router.post('/login', async (req, res, next) => {
     try{
         const user = await User.findOne({email: req.body.email});
         if(!user){
-            return next(createError(404, "User not found"));
+            res.status(404).send("User not Found!");
         }
         const isPasswordValid = await bcrypt.compare(req.body.password, user.password);
         if(!isPasswordValid){
-            return next(createError(404, "Invalid credentials"));
+            res.status(404).send("Invalid Credentials!");
         }
-        const token = jwt.sign({_id: user._id}, process.env.JWT_SECRET);
+        const token = jwt.sign({_id: user._id}, `${process.env.JWT_SECRET}`);
         const {password , ...userWithoutPassword} = user.toObject();
         res
         .cookie("access_token", token, {
@@ -47,7 +47,7 @@ router.post('/googleauth',async (req, res, next) => {
     try{
         const user = await User.findOne({email: req.body.email});
         if(user){
-            const token = jwt.sign({_id: user._id}, process.env.JWT_SECRET);
+            const token = jwt.sign({_id: user._id},`${process.env.JWT_SECRET}`);
             res
             .cookie("access_token", token, {
                 httpOnly: true
@@ -59,7 +59,7 @@ router.post('/googleauth',async (req, res, next) => {
             fromGoogle: true,
             })
            const savedUser = await newUser.save(); 
-           const token = jwt.sign({_id: savedUser._id}, process.env.JWT_SECRET);
+           const token = jwt.sign({_id: savedUser._id}, `${process.env.JWT_SECRET}`);
            res
            .cookie("access_token", token, {
                httpOnly: true
