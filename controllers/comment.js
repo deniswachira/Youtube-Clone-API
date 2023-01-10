ee4b0c83c31e45aac206063ae98effae966b5a47
Denis Wachira <deniswachira77@gmail.com>
2023-01-10 23:21:10 +0300

    done

--- controllers/comment.js
+++ controllers/comment.js
@@ -19,7 +19,7 @@ const deleteComment = async (req, res, next) =>{
         const video = await Video.findById(res.params.id)
         if(req.user.id === comment.userId || req.user.id === video.userId){
             await Comment.findByIdAndDelete(req.params.id)
-            res.status(200).json("The comment has been deleted. ")
+            res.status(200).json("Comment has been deleted. ")
         }else{
             return next(createError(404, "You can only delete your comment"))
         }
